483de39a0b6036cac17da205dbb667cbd0869d14
Mahika <marathemahika11@gmail.com>
2026-04-16 23:48:54 +0530

    push

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config(); // Nodemon trigger
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
